d38d822fbdf6c292404f87b1a8958e79ea7722b9
aktsbot <box.ashishk@gmail.com>
2019-12-29 22:33:36 +0530

    adjust viewport size

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ const makePage = (color) => {
     <!DOCTYPE html>
     <html>
       <head>
+        <meta charset="UTF-8" />                                                      
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>${title}</title>
       </head>
       <body style="background-color:#${color}">
